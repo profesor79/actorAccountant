ca4b44bfac95d77c3fad224d8269811404624804
Greg <you@example.com>
2022-09-17 18:31:17 +0200

    added actor and first test

--- actor.tests/AccountantActorTests.cs
+++ actor.tests/AccountantActorTests.cs
@@ -1,3 +1,5 @@
+using actors;
+using Akka.Actor;
 using Akka.TestKit.Xunit2;
 
 namespace actor.tests;
@@ -8,6 +10,16 @@ public class AccountantActorTests:ActorTestBase
     [Fact]
     public void Test1()
     {
+        //given accountant actor
+        var props = Props.Create(() => new AccountantActor());
+        _sut = Sys.ActorOf(props);
 
+        // when we ask for a balance        
+        _sut.Tell(new AccountantActor.GetBalanceMessage());
+
+        //then balance message shall be received
+        ExpectMsg<AccountantActor.CurrentBalanceMessage>(m => {
+            Assert.Equal(0, m.Balance);
+        });
     }
 }
--- actors/AccountantActor.cs
+++ actors/AccountantActor.cs
@@ -4,8 +4,30 @@ namespace actors;
 public class AccountantActor:ReceiveActor
 {
 
+	decimal _balance = 0;
 	public AccountantActor()
 	{
+		Receive<GetBalanceMessage>(SendCurrentBalance);
+	}
 
+	private void SendCurrentBalance(GetBalanceMessage m)
+	{
+		Console.WriteLine($"sending balance: {_balance}");
+		Sender.Tell(new CurrentBalanceMessage(_balance));	
+	}
+
+	public class GetBalanceMessage
+	{
+		
+	}
+
+	public class CurrentBalanceMessage
+	{
+		public decimal Balance { get; set; }
+
+		public CurrentBalanceMessage(decimal balance)
+		{
+			Balance = balance;
+		}
 	}
 }
